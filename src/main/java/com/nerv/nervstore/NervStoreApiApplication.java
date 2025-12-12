97f1924899cfe1106f52bf8a510e1b0e181521c0
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-12-12 20:52:12 -0300

    Initial commit: estrutura base do projeto Spring Boot

---
+++ src/main/java/com/nerv/nervstore/NervStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.nerv.nervstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NervStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NervStoreApiApplication.class, args);
+	}
+
+}
